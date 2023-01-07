69f5b4b67cdd2948352ee65f3bb4b5490d33a228
YutongSheng <544518449@qq.com>
2023-01-07 11:29:54 +0800

    feat: 添加路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import ColumnDetail from "@/views/ColumnDetail.vue";
 import CreatePost from "@/views/CreatePost.vue";
+import store from "@/store";
 const routerHistory = createWebHistory();
 export const router = createRouter({
   history: routerHistory,
@@ -16,6 +17,7 @@ export const router = createRouter({
       path: "/login",
       name: "login",
       component: Login,
+      meta: { redirectAlreadyLogin: true },
     },
     {
       path: "/column/:id",
@@ -26,6 +28,22 @@ export const router = createRouter({
       path: "/create",
       name: "create",
       component: CreatePost,
+      meta: { requiredLogin: true },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to.meta.redirectAlreadyLogin);
+  console.log(store.state.user.isLogin);
+
+  if (!store.state.user.isLogin && to.meta.requiredLogin) {
+    next({
+      name: "login",
+    });
+  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
+    next("/");
+  } else {
+    next();
+  }
 });
